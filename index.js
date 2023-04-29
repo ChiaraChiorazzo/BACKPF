9526c8d129ea1b4f5f0f223dd8ca0d3499817b2b
ChiaraChiorazzo <chiarachiorazzo1@gmail.com>
2023-04-29 20:44:45 +0200

    Third commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+const { PORT } = process.env;
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const createRole = require("./src/helpers/createRoles/createRoles.js");
 const createUser = require("./src/helpers/createUser/createUser.js")
 
+
 conn.sync({ after: true }).then(async () => {
   await createRole();
   await createUser();
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001');
   });
 });
