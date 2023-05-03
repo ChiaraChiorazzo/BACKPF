48c34c4f284a6f8d8ba2de0908c51cc32611e065
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-05-03 20:02:53 -0300

    Se descomentaron los mails

--- src/controllers/authController/authController.js
+++ src/controllers/authController/authController.js
@@ -1,50 +1,66 @@
 const generateTokenJwt = require('../../helpers/tokenjwt/generateTokenJwt');
 const handlerbars = require('handlebars');
-const {encrypt} = require("../../helpers/password/bcryptHelper")
+const { encrypt } = require('../../helpers/password/bcryptHelper');
 const fs = require('fs');
 const { transporter } = require('../../utils/mailer');
 const path = require('path');
 const util = require('util');
-const { User, Rol } = require("../../db")
+const { User, Rol } = require('../../db');
 
 const authGoogle = async ({ name, surname, mail, image }) => {
   console.log(image, name);
-    try {
-      const passwordHash = await encrypt(`${mail}${process.env.JWT_SECRET}`);
+  try {
+    const passwordHash = await encrypt(`${mail}${process.env.JWT_SECRET}`);
 
-        const roles = await Rol.findOne({ where: { rol_name: 'user' } });
+    const roles = await Rol.findOne({ where: { rol_name: 'user' } });
 
-        const [ user, created ] = await User.findOrCreate({
-            where: { mail },
-            defaults: {
-                name, 
-                surname,
-                image, 
-                password: passwordHash,
-                mail,
-                phone: "11222333",
-                date_of_birth: "2000-01-01",
-                rolId: roles.id
-            }
-        });
+    const [user, created] = await User.findOrCreate({
+      where: { mail },
+      defaults: {
+        name,
+        surname,
+        image,
+        password: passwordHash,
+        mail,
+        phone: '11222333',
+        date_of_birth: '2000-01-01',
+        rolId: roles.id,
+      },
+    });
 
-        const token = await generateTokenJwt(user);
+    const token = await generateTokenJwt(user);
 
-        if(!created) {
-          return {
-            message: "User in login",
-            token
-          }
-        };
-
-        return {
-          message: "User created",
-          token
-        }
-
-    } catch (error) {
-      throw new Error(error.message);
+    if (!created) {
+      return {
+        message: 'User in login',
+        token,
+      };
     }
+
+    const readFile = util.promisify(fs.readFile);
+    const templateFile = await readFile(
+      path.resolve(__dirname, '../../views/emailWelcomeGoogle.handlebars'),
+      'utf8'
+    );
+    const template = handlerbars.compile(templateFile);
+    const html = template({ name });
+
+    const mailOptions = {
+      from: 'the.chocolate.hub@outlook.com',
+      to: mail,
+      subject: 'Forgot password',
+      html,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return {
+      message: 'User created',
+      token,
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 // const authGoogle = async (user) => {
@@ -53,20 +69,20 @@ const authGoogle = async ({ name, surname, mail, image }) => {
 
 //         const { name, mail, surname } = user;
 
-//         // Se agrega el envío del correo cuando se registra por google
-//         // const readFile = util.promisify(fs.readFile);
-//         // const templateFile = await readFile(path.resolve(__dirname, '../../views/emailWelcomeGoogle.handlebars'), 'utf8');
-//         // const template = handlerbars.compile(templateFile);
-//         // const html = template({ name })
+// Se agrega el envío del correo cuando se registra por google
+// const readFile = util.promisify(fs.readFile);
+// const templateFile = await readFile(path.resolve(__dirname, '../../views/emailWelcomeGoogle.handlebars'), 'utf8');
+// const template = handlerbars.compile(templateFile);
+// const html = template({ name })
 
-//         // const mailOptions = {
-//         //     from: 'the.chocolate.hub@outlook.com',
-//         //     to: mail,
-//         //     subject: 'Forgot password',
-//         //     html,
-//         //   };
+// const mailOptions = {
+//     from: 'the.chocolate.hub@outlook.com',
+//     to: mail,
+//     subject: 'Forgot password',
+//     html,
+//   };
 
-//         // await transporter.sendMail(mailOptions);
+// await transporter.sendMail(mailOptions);
 
 //         return {
 //             message: "User created successfully",
